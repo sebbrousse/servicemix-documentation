51675a50e3c334d850d43c5009d2572c3673e25d
emjohnson <emjohnson@fusesource.com>
2010-11-08 10:43:50 -0500

    got scalate working and updated styling to match new site design

--- src/main/scala/scalate/Boot.scala
+++ src/main/scala/scalate/Boot.scala
@@ -18,9 +18,33 @@
 package scalate
 
 import org.fusesource.scalate.util.Logging
+import java.util.concurrent.atomic.AtomicBoolean
+import org.fusesource.scalate.TemplateEngine
+import org.fusesource.scalamd.{MacroDefinition, Markdown}
+import java.util.regex.Matcher
+import org.fusesource.scalate.wikitext.Pygmentize
 
-class Boot extends Logging {
+class Boot(engine: TemplateEngine) extends Logging {
 
-  def run = debug("Boot class started")
+  private var _initialised = new AtomicBoolean(false)
 
+  def run: Unit = {
+    if (_initialised.compareAndSet(false, true)) {
+
+      def pygmentize(m:Matcher):String = Pygmentize.pygmentize(m.group(2), m.group(1))
+
+      // add some macros to markdown.
+      Markdown.macros :::= List(
+        MacroDefinition("""\{pygmentize::(.*?)\}(.*?)\{pygmentize\}""", "s", pygmentize, true),
+        MacroDefinition("""\{pygmentize\_and\_compare::(.*?)\}(.*?)\{pygmentize\_and\_compare\}""", "s", pygmentize, true)
+        )
+
+      for( ssp <- engine.filter("ssp"); md <- engine.filter("markdown") ) {
+        engine.pipelines += "ssp.md"-> List(ssp, md)
+        engine.pipelines += "ssp.markdown"-> List(ssp, md)
+      }
+    }
+    
+    debug("Boot class started")
+  }
 }
