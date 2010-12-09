0fad3479028985e81159e859894ecdf78341e8e2
Eric Johnson <emjohnson@fusesource.com>
2010-12-09 10:34:36 -0500

    Add sample {snippet} and a few minor fixes
    git-svn-id: https://svn.apache.org/repos/asf/servicemix/documentation/trunk@1043424 13f79535-47bb-0310-9956-ffa450edef68
    
    Conflicts:
    
    	pom.xml
    	src/main/scala/scalate/Boot.scala
    	src/main/webapp/jbi/components/servicemix-eip.conf

--- src/main/scala/scalate/Boot.scala
+++ src/main/scala/scalate/Boot.scala
@@ -18,7 +18,7 @@
 package scalate
 
 import org.fusesource.scalate.util.Logging
-import java.util.concurrent.atomic.AtomicBoolean
+import org.fusesource.scalate.wikitext.Snippets
 import org.fusesource.scalate.TemplateEngine
 import org.fusesource.scalamd.{MacroDefinition, Markdown}
 import java.util.regex.Matcher
@@ -26,25 +26,9 @@ import org.fusesource.scalate.wikitext.Pygmentize
 
 class Boot(engine: TemplateEngine) extends Logging {
 
-  private var _initialised = new AtomicBoolean(false)
-
-  def run: Unit = {
-    if (_initialised.compareAndSet(false, true)) {
-
-      def pygmentize(m:Matcher):String = Pygmentize.pygmentize(m.group(2), m.group(1))
-
-      // add some macros to markdown.
-      Markdown.macros :::= List(
-        MacroDefinition("""\{pygmentize::(.*?)\}(.*?)\{pygmentize\}""", "s", pygmentize, true),
-        MacroDefinition("""\{pygmentize\_and\_compare::(.*?)\}(.*?)\{pygmentize\_and\_compare\}""", "s", pygmentize, true)
-        )
-
-      for( ssp <- engine.filter("ssp"); md <- engine.filter("markdown") ) {
-        engine.pipelines += "ssp.md"-> List(ssp, md)
-        engine.pipelines += "ssp.markdown"-> List(ssp, md)
-      }
-    }
-    
+  def run = {
     debug("Boot class started")
+    Snippets.addPrefix("servicemix", "http://svn.apache.org/repos/asf/servicemix")
   }
+
 }
